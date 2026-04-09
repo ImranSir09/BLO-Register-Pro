e43e8d8250857241bb4bee0ecc9d9eeae0effaf2
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-10 00:56:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "https://esm.sh/react@19"
 import ReactDOM from "https://esm.sh/react-dom@19/client"
-import App from "https://esm.sh/gh/ImranSir09/BLO-Register-Pro/App.tsx"
+import App from "https://esm.sh/gh/ImranSir09/BLO-Register-Pro@main/App.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 React.createElement(App)
